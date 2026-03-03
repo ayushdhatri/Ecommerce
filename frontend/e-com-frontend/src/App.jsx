98756a033aad8d6d9ebcc62e64eec60cbcd1d95f
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-04 03:00:00 +0530

    Added Navbar to our page

--- frontend/e-com-frontend/src/App.jsx
+++ frontend/e-com-frontend/src/App.jsx
@@ -6,11 +6,13 @@ import { FaBeer } from 'react-icons/fa';
 import Products from './components/products/Products';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
+import Navbar from './components/shared/Navbar';
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
     <Router>
+      <Navbar/>
       <Routes>
         <Route path = '/' element = {<Home/>}/>
         <Route path = '/products' element ={<Products/>}/>
