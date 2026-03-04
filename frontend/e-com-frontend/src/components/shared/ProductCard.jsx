bb157fbf7bb69142790b719c608a2c90f875e3af
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-04 23:18:08 +0530

    Adding Contact Section

--- frontend/e-com-frontend/src/components/shared/ProductCard.jsx
+++ frontend/e-com-frontend/src/components/shared/ProductCard.jsx
@@ -68,38 +68,37 @@ const ProductCard = ({
           <div className="min-h-20 max-h-20">
             <p>{description}</p>
           </div>
-          { !about && (
+          {!about && (
             <div className="flex items-center justify-between">
-            {specialPrice ? (
-              <div className="flex flex-col">
-                <span className="text-gray-400 line-through">
-                  {Number(price).toFixed(2)}
-                </span>
-                <span className="text-xl font-bold text-slate-700">
-                  {Number(price).toFixed(2)}
-                </span>
-              </div>
-            ) : (
-              <div className="flex flex-col">
-                <span className="text-xl font-bold text-slate-700">
-                  {Number(price).toFixed(2)}
-                </span>
-              </div>
-            )}
+              {specialPrice ? (
+                <div className="flex flex-col">
+                  <span className="text-gray-400 line-through">
+                    {Number(price).toFixed(2)}
+                  </span>
+                  <span className="text-xl font-bold text-slate-700">
+                    {Number(price).toFixed(2)}
+                  </span>
+                </div>
+              ) : (
+                <div className="flex flex-col">
+                  <span className="text-xl font-bold text-slate-700">
+                    {Number(price).toFixed(2)}
+                  </span>
+                </div>
+              )}
 
-            <button
-              disabled={!isAvailable || btnLoader}
-              className={`bg-blue-500 ${
-                isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-80"
-              }
+              <button
+                disabled={!isAvailable || btnLoader}
+                className={`bg-blue-500 ${
+                  isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-80"
+                }
               text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}
-            >
-              <FaShoppingCart className="mr-2" />
-              {isAvailable ? "Add to Cart" : "Stock Out"}
-            </button>
-          </div>
-          ) }
-          
+              >
+                <FaShoppingCart className="mr-2" />
+                {isAvailable ? "Add to Cart" : "Stock Out"}
+              </button>
+            </div>
+          )}
         </div>
         <ProductViewModal
           open={openProductdViewModal}
