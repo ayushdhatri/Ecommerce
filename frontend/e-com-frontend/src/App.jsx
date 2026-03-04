e0e7c8169804d19c2a427c34464c358843db1fb5
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-05 02:11:37 +0530

    Adding Cart Page Component Design

--- frontend/e-com-frontend/src/App.jsx
+++ frontend/e-com-frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Home from './components/home/Home';
 import Navbar from './components/shared/Navbar';
 import About from './components/About';
 import Contact from './components/Contact';
+import Cart from './components/cart/Cart';
 import { Toaster } from "react-hot-toast";
 function App() {
   const [count, setCount] = useState(0)
@@ -22,6 +23,8 @@ function App() {
         <Route path = '/products' element ={<Products/>}/>
         <Route path = '/about' element={<About/>} />
         <Route path = '/contact' element={<Contact/>} />
+        <Route path = '/cart' element={<Cart/>}/>
+        
       </Routes>
      
     </Router>
