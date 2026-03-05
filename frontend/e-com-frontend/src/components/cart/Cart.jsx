813dc6eb02f6254c6a08ed330ebb23f5d49d7749
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-06 00:54:19 +0530

    Adding Implementation for designing the Cart Section

--- frontend/e-com-frontend/src/components/cart/Cart.jsx
+++ frontend/e-com-frontend/src/components/cart/Cart.jsx
@@ -1,7 +1,20 @@
-import { MdShoppingCart, MdArrowBack  } from "react-icons/md";
+import { MdShoppingCart, MdArrowBack } from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import ItemContent from "../cart/ItemContent";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 const Cart = () => {
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state.carts);
+  const newCart = { ...cart };
+  newCart.totalPrice = cart.reduce((acc, curr) => {
+    let itemPrice = curr.specialPrice
+      ? Number(curr.specialPrice)
+      : Number(curr.price);
+    return acc + itemPrice * Number(curr?.quantity);
+  }, 0);
+
+  if(!cart || cart.length === 0)return <h1>Cart Is Empty</h1>
   return (
     <div className="lg:px-14 sm:px-8 px-4 py-10">
       <div className="flex flex-col items-center mb-12">
@@ -11,8 +24,8 @@ const Cart = () => {
         </h1>
         <p className="text-lg text-gray-600 mt-2">All Your selected Items</p>
       </div>
-      <div className="grid md:grid-cols-5 grid-cols-4 gap-4 pb-2 font-semibold items-center">
-        <div className="md:cols-span-2 justify-self-start text-lg text-slate-800 lg:ps-4">
+      <div className="grid md:grid-cols-5 grid-cols-4 gap-5 pb-2 font-semibold items-center">
+        <div className="md:col-span-2 justify-self-start text-lg text-slate-800 lg:px-4">
           Product
         </div>
         <div className="justify-self-center text-lg text-slate-800">Price</div>
@@ -21,6 +34,15 @@ const Cart = () => {
         </div>
         <div className="justify-self-center text-lg text-slate-800">Total</div>
       </div>
+
+      <div>
+        {cart &&
+          cart.length > 0 &&
+          cart.map((item, i) => {
+            return <ItemContent key={i} {...item} />;
+          })}
+      </div>
+
       <div className="border-t-[1.5px] bprder-slate-200 y-4 flex sm:felx-row sm:px-0 px-2 flex-col sm:justfiy-between gap-4">
         <div></div>
         <div className="flex text-sm gap-1 flex-col">
@@ -28,19 +50,22 @@ const Cart = () => {
             <span>Subtotal</span>
             <span>Rs-400</span>
           </div>
-          <p className="text-slate-500">Taxes and Shipping calculated at checkout</p>
-          <Link className="w-full flex justify-end" to ="/checkout">
-          <button
-            onClick={()=>{}} 
-            className="font-semibold w-[300px] py-2 px-4 rounded-sm bg-custom-blue text-white flex items-center justify-center gap-2 hover:text-gray-300 transition duration-500"
-          >checkout</button>
+          <p className="text-slate-500">
+            Taxes and Shipping calculated at checkout
+          </p>
+          <Link className="w-full flex justify-end" to="/checkout">
+            <button
+              onClick={() => {}}
+              className="font-semibold w-[300px] py-2 px-4 rounded-sm bg-custom-blue text-white flex items-center justify-center gap-2 hover:text-gray-300 transition duration-500"
+            >
+              checkout
+            </button>
           </Link>
           <Link className="flex gap-2 items-center mt-2" to="/products">
-          <MdArrowBack />
-          <span>Continue Shopping</span>
+            <MdArrowBack />
+            <span>Continue Shopping</span>
           </Link>
         </div>
-        
       </div>
     </div>
   );
