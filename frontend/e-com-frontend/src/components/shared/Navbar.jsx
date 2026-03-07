9a51afee554f8276b418885141f9d4eaa4acd223
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-07 23:36:53 +0530

    Adding modification in navbar to show dynamically if user is logged in or not

--- frontend/e-com-frontend/src/components/shared/Navbar.jsx
+++ frontend/e-com-frontend/src/components/shared/Navbar.jsx
@@ -10,6 +10,9 @@ const Navbar = () => {
   const path = useLocation().pathname;
   const [navbarOpen, setNavbarOpen] = useState(false);
   const {cart} = useSelector((state) => state.carts);
+  const { user } = useSelector((state)=> state.auth);
+
+
 
   return (
     <div className="h-17.5 bg-custom-gradient text-white z-50 flex items-center sticky top-0">
@@ -68,6 +71,11 @@ const Navbar = () => {
               </Badge>
             </Link>
           </li>
+          {user && user.id ? (
+            <li className="font-medium transition-all duration-150">
+              <p className="text-white">Welcome</p>
+            </li>
+          ) : (
           <li className="font-medium transition-all duration-150">
             <Link
               className="flex items-center space-x-2 px-4 py-[6px] 
@@ -81,6 +89,7 @@ const Navbar = () => {
               <span>Login</span>
             </Link>
           </li>
+          )}
         </ul>
         <button
           onClick={() => setNavbarOpen(!navbarOpen)}
@@ -96,4 +105,4 @@ const Navbar = () => {
     </div>
   );
 };
-export default Navbar;
+export default Navbar;1
