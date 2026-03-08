5a799fcf36f0defdc29f7fe325d41e46ccdf04f3
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-08 17:43:16 +0530

    Adding addressModal

---
+++ frontend/e-com-frontend/src/components/checkout/AddAddressForm.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { InputField } from "../shared/InputField";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { FaAddressCard } from "react-icons/fa";
+import { useSelector } from "react-redux";
+const AddAddressForm = () => {
+  const { btnLoader } = useSelector((state) => state.errors);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [loader, setLoader] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    mode: "onTouched",
+  });
+
+  const onSaveAddressHandler = async (data) => {
+    console.log("Save Address Clicked");
+  };
+
+  return (
+    <div className="min-h-[calc(100vh-64px)] flex justify-center items-center bg-white">
+      <form onSubmit={handleSubmit(onSaveAddressHandler)}>
+        <div className="flex justify-center items-center mb-4 font-semibold text-2xl text-slate-800 py-2 px-4">
+          <FaAddressCard className="mr-2 text-2xl" />
+          Add Address
+        </div>
+        <div className="flex flex-col gap-4">
+          <InputField
+            message="*Building Name is required"
+            label="Building"
+            required
+            id="building"
+            type="text"
+            register={register}
+            errors={errors}
+            placeholder="Enter Your Building Name"
+          />
+          <InputField
+            message="*City is required"
+            label="city"
+            required
+            id="city"
+            type="text"
+            register={register}
+            errors={errors}
+            placeholder="Enter Your city"
+          />
+          <InputField
+            message="*Pincode is required"
+            label="pincode"
+            required
+            id="pincode"
+            type="text"
+            register={register}
+            errors={errors}
+            placeholder="Enter Your Pincode"
+          />
+          <InputField
+            message="*Street is required"
+            label="street"
+            required
+            id="street"
+            type="text"
+            register={register}
+            errors={errors}
+            placeholder="Enter Your Street"
+          />
+          <InputField
+            message="*Country is required"
+            label="country"
+            required
+            id="country"
+            type="text"
+            register={register}
+            errors={errors}
+            placeholder="Enter Your Country"
+          />
+          <button disabled={btnLoader} className=" text-white bg-custom-blue px-4 py-2" type="submit">
+            {btnLoader ? (
+              <>
+                <Spinners /> Loading...
+              </>
+            ) : (
+              <>Save</>
+            )}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddAddressForm;
--- frontend/e-com-frontend/src/components/checkout/AddressInfo.jsx
+++ frontend/e-com-frontend/src/components/checkout/AddressInfo.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import Skeleton from "../shared/Skeleton";
 import {FaAddressBook} from 'react-icons/fa'
+import {useState} from 'react';
+import AddressInfoModal from './AddressInfoModal';
+import AddAddressForm from './AddAddressForm';
 const AddressInfo = () => {
+    const [openAddressModal, setOpenAddressModal] = useState(false);
+    const [selectedAddress, setSelectedAddres] = useState("");
+    const addNewAddressHandler = ()=>{
+        setSelectedAddres("");
+        setOpenAddressModal(true);
+
+
+
+    }
   const noAddressExist = true;
   const isLoading = true;
   return (
@@ -15,6 +27,11 @@ const AddressInfo = () => {
             <p className="md-2 text-slate-400 text-center font-semibold my-4">
                 Please Add your address to complete purchase
             </p>
+            <button className="px-4 py-2 bg-blue-600 text-white font-medium rounded hover:bg-blue-700 transition-all"
+                onClick={addNewAddressHandler}
+            >
+                Add Address
+            </button>
             
         </div>
       ) : (
@@ -35,6 +52,13 @@ const AddressInfo = () => {
           </div>
         </div>
       )}
+
+      <AddressInfoModal 
+        open = {openAddressModal}
+        setOpen={setOpenAddressModal} >
+            <AddAddressForm/>
+        </AddressInfoModal>
+
     </div>
   );
 };
