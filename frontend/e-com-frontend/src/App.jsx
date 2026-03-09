a9501a8fe08133d3dab14a67460d17a769fc55d5
Ayush Solanki <aayushdhatri@gmail.com>
2026-03-10 00:18:50 +0530

    Added Private Route to checkout

--- frontend/e-com-frontend/src/App.jsx
+++ frontend/e-com-frontend/src/App.jsx
@@ -27,9 +27,9 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-
+          <Route path="/cart" element={<Cart />} />
           <Route path="/" element={<PrivateRoute />}>
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
           </Route>
 
           <Route path="/" element={<PrivateRoute publicPage />}>
